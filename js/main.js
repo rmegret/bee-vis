9587245ece8be2ae70b61e4e1a595281d8a69c45
PoaiPabloM <64501026+PoaiPabloM@users.noreply.github.com>
2025-05-22 00:19:47 +0000

    Final Submission

--- js/main.js
+++ js/main.js
@@ -13,34 +13,6 @@ function convert_columns_to_number(data, columns) {
     }
   }
 }
-// Convert a flat list of rows into an indexed dictionnary
-// [{a:1, b:30}, {a:10, b:40}] => {1: {a:1, b:30}, 10: {a:10, b:40}}
-// The new structure points to the old structure, it just index it
-// So that any modification to the items of `indexed_data` modifies `data`
-function index_table(data, index_column) {
-  indexed_data = {}
-  for (row of data) {
-    indexed_data[row[index_column]] = row // Allow indexed access to the data
-  }
-  return indexed_data
-}
-
-// Function to be used in list.sort() to sort by multiple fields
-// in a list of objects [{a:1, b:10},{a:2,b:30}...]
-function sortByMultipleProperties(properties) {
-  return function (a, b) {
-    for (let prop of properties) {
-      let dir = 1;
-      if (prop[0] === '-') {
-        dir = -1;
-        prop = prop.slice(1);
-      }
-      if (a[prop] < b[prop]) return -1 * dir;
-      if (a[prop] > b[prop]) return 1 * dir;
-    }
-    return 0;
-  };
-}
 
 // pablo Muñoz flower patch visualization (added comment to push)
 async function show_flowerpatch_visualization(){
